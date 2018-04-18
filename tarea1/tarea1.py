b524cb7ac49b0cc148b64d55bb77f63c5be80ab6
lpavezb <lukas.pavez@ug.uchile.cl>
2018-04-17 23:57:44 -0300

    still working hard

--- tarea1/tarea1.py
+++ tarea1/tarea1.py
@@ -7,6 +7,7 @@ Esto hace tarea
 
 # Importar librería
 import matplotlib.pyplot as plt  # grafico
+import matplotlib.colors as color
 
 import tqdm
 import numpy as np
@@ -25,6 +26,8 @@ class Tarea:
         self._ancho = ancho  # privada
         self._largo = largo
         self._dh = dh
+
+        self._t = 0  # hora
 
         self._h = int(float(ancho) / dh)
         self._w = int(float(largo) / dh)
@@ -145,8 +148,7 @@ class Tarea:
                     self._matrix[y, x] = 3
 
     def cb(self, t):
-        if t > 24:
-            t = 0
+        self._t = t
 
         # puntos
         cielo = 0
@@ -181,8 +183,31 @@ class Tarea:
                 elif self._matrix[i, j] == fabrica:
                     self._temp[i, j] = t_fab
 
+    def plot_geo(self):
+        fig = plt.figure()
+
+        cmap = color.ListedColormap(['c', 'b', 'k', 'brown', 'w'])
+        bounds = [0, 1, 2, 3, 4, 5]
+        norm = color.BoundaryNorm(bounds, cmap.N)
+        cax = plt.imshow(self._geo, origin="lower", cmap=cmap, norm=norm)
+        cb = fig.colorbar(cax, cmap=cmap, norm=norm, boundaries=bounds, ticks=[0.5, 1.5, 2.5, 3.5, 4.5])
+        cb.ax.set_yticklabels(['atmosfera', 'mar', 'fabrica', 'montana', 'nieve'])
+        plt.title("Geografia")
+        plt.xlabel("Ancho [m]")
+        plt.ylabel("Altura [m]")
+        plt.show()
+
+    def plot_temp(self):
+        fig = plt.figure()
+        cax = plt.imshow(self._temp, origin="lower")
+        fig.colorbar(cax)
+        plt.title("Temperatura a las " + str(self._t) + ":00 hrs")
+        plt.xlabel("Ancho [m]")
+        plt.ylabel("Altura [m]")
+        plt.show()
+
     def get_cb_matrix(self):
-        return self._matrix
+            return self._matrix
 
     def get_temp_matrix(self):
         return self._temp
@@ -197,100 +222,115 @@ class Tarea:
         ni = 0  # numero de iteraciones
         w = self._w
         h = self._h
-        while ni <= 1000:
+        for _ in tqdm.tqdm(range(1000)):
             for j in range(w):
                 for i in range(h):
-                    (self._temp[i][j], e) = sobreRS(self._temp, i, j, get_w(w - 1, h - 1), w - 1, h - 1, self._dh, rho,
-                                                    b)
-            print e
-            if e < epsilon: break
-            if e0 == 0: e0 = e
+                    (self._temp[i][j], e) = sobreRS(self._temp, i, j, get_w(w - 1, h - 1), e, w - 1, h - 1,
+                                                    self._dh, rho, b)
+            print(e)
+            if e < epsilon:
+                break
+            if e0 == 0:
+                e0 = e
             ni += 1  # numero de iteraciones
-            print ni
 
 
 # Funcion copiada de https://github.com/ppizarror/01-tarea-computagrafica/blob/master/lib.py
 # Funcion que retorna true si hay un NAN en torno a a[i][j]
 def nearNAN(a, i, j):
-    if np.isnan(a[i - 1][j]): return True
-    if np.isnan(a[i + 1][j]): return True
-    if np.isnan(a[i][j - 1]): return True
-    if np.isnan(a[i][j + 1]): return True
+    if np.isnan(a[i - 1][j]):
+        return True
+    if np.isnan(a[i + 1][j]):
+        return True
+    if np.isnan(a[i][j - 1]):
+        return True
+    if np.isnan(a[i][j + 1]):
+        return True
     return False
 
 
-def sobreRS(matriz, i, j, w, width, height, h, rho, b=False):
+def sobreRS(matriz, i, j, w, e, width, height, h, rho, b=False):
+    if b:
+        ro = rho(i, j)
+    else:
+        ro = 0
+    if np.isnan(matriz[i][j]):
+        return matriz[i][j], e
     if 0 < i < height and 0 < j < width:
-        if np.isnan(matriz[i][j]):
-            n = matriz[i][j]
-        elif nearNAN(matriz, i, j):
-            if np.isnan(matriz[i][j + 1]):
-                if np.isnan(matriz[i + 1][j]):
+        if nearNAN(matriz, i, j):
+            if np.isnan(matriz[i][j + 1]):  # derecha
+                if np.isnan(matriz[i + 1][j]):  # derecha y abajo
                     n = matriz[i][j] + (1.0 / 4) * (2 * matriz[i - 1][j] + 2 * matriz[i][j - 1] -
-                                                    4 * matriz[i][j]) * w
+                                                    4 * matriz[i][j] - (h ** 2) * ro) * w
                 else:
-                    if np.isnan(matriz[i - 1][j]):
-                        if np.isnan(matriz[i][j - 1]):
-                            n = matriz[i][j] + (1.0 / 4) * (4 * matriz[i + 1][j] - 4 * matriz[i][j]) * w
+                    if np.isnan(matriz[i - 1][j]):  # derecha y arriba
+                        if np.isnan(matriz[i][j - 1]):  # derecha, arriba y izquierda
+                            n = matriz[i][j] + (1.0 / 4) * (4 * matriz[i + 1][j] - 4 * matriz[i][j] -
+                                                            (h ** 2) * ro) * w
+
                         else:
-                            n = matriz[i][j] + (1.0 / 4) * (2 * matriz[i + 1][j] + 2 *
-                                                            matriz[i][j - 1] - 4 * matriz[i][j]) * w
+                            n = matriz[i][j] + (1.0 / 4) * (2 * matriz[i + 1][j] + 2 * matriz[i][j - 1] -
+                                                            4 * matriz[i][j] - (h ** 2) * ro) * w
+
                     else:
-                        n = matriz[i][j] + (1.0 / 4) * (matriz[i + 1][j] + matriz[i - 1][j] + 2 *
-                                                        matriz[i][j - 1] - 4 * matriz[i][j]) * w
-            elif np.isnan(matriz[i + 1][j]):
-                if np.isnan(matriz[i][j - 1]):
+                        n = matriz[i][j] + (1.0 / 4) * (matriz[i + 1][j] + matriz[i - 1][j] +
+                                                        2 * matriz[i][j - 1] - 4 * matriz[i][j] - (h ** 2) * ro) * w
+
+            elif np.isnan(matriz[i + 1][j]):  # abajo
+                if np.isnan(matriz[i][j - 1]):  # izquierda
                     n = matriz[i][j] + (1.0 / 4) * (2 * matriz[i - 1][j] + 2 * matriz[i][j + 1] -
-                                                    4 * matriz[i][j]) * w
+                                                    4 * matriz[i][j] - (h ** 2) * ro) * w
+
                 else:
-                    n = matriz[i][j] + (1.0 / 4) * (2 * matriz[i - 1][j] + matriz[i][j + 1] +
-                                                    matriz[i][j - 1] - 4 * matriz[i][j]) * w
-            elif np.isnan(matriz[i][j - 1]):
+                    n = matriz[i][j] + (1.0 / 4) * (2 * matriz[i - 1][j] + matriz[i][j + 1] + matriz[i][j - 1] -
+                                                    4 * matriz[i][j] - (h ** 2) * ro) * w
+
+            elif np.isnan(matriz[i][j - 1]):  # izquierda
                 if np.isnan(matriz[i - 1][j]):
-                    n = matriz[i][j] + (1.0 / 4) * (2 * matriz[i + 1][j] + 2 *
-                                                    matriz[i][j + 1] - 4 * matriz[i][j]) * w
+                    n = matriz[i][j] + (1.0 / 4) * (2 * matriz[i + 1][j] + 2 * matriz[i][j + 1] -
+                                                    4 * matriz[i][j] - (h ** 2) * ro) * w
+
                 else:
-                    n = matriz[i][j] + (1.0 / 4) * (matriz[i + 1][j] + matriz[i - 1][j] + 2 *
-                                                    matriz[i][j + 1] - 4 * matriz[i][j]) * w
-            else:
-                return (matriz[i][j], 1)
+                    n = matriz[i][j] + (1.0 / 4) * (matriz[i + 1][j] + matriz[i - 1][j] + 2 * matriz[i][j + 1] -
+                                                    4 * matriz[i][j] - (h ** 2) * ro) * w
+
+            else:  # arriba
+                n = matriz[i][j] + (1.0 / 4) * (matriz[i + 1][j] + matriz[i][j-1] + 2 *
+                                                matriz[i][j + 1] - 4 * matriz[i][j] - (h ** 2) * ro) * w
+
         else:
             n = matriz[i][j] + (1.0 / 4) * (matriz[i + 1][j] + matriz[i - 1][j] + matriz[i][j + 1] + matriz[i][j - 1] -
-                                            4 * matriz[i][j] - (h ** 2) * rho(i, j, b)) * w
+                                            4 * matriz[i][j] - (h ** 2) * ro) * w
     else:
         # Esquinas
         if i == 0 and j == 0:  # esquina superior izquierda
-            n = matriz[i][j] + (1.0 / 4) * (2 * matriz[i + 1][j] + 2 * matriz[i][j + 1] - 4 * matriz[i][j]) * w
+            n = matriz[i][j] + (1.0 / 4) * (2 * matriz[i + 1][j] + 2 * matriz[i][j + 1] - 4 * matriz[i][j]
+                                            - (h ** 2) * ro) * w
+
         elif i == 0 and j == width:  # esquina superior derecha
-            n = matriz[i][j] + (1.0 / 4) * (2 * matriz[i + 1][j] + 2 * matriz[i][j - 1] - 4 * matriz[i][j]) * w
+            n = matriz[i][j] + (1.0 / 4) * (2 * matriz[i + 1][j] + 2 * matriz[i][j - 1] - 4 * matriz[i][j]
+                                            - (h ** 2) * ro) * w
+
         elif i == height and j == 0:  # esquina inferior izquierda
-            n = matriz[i][j] + (1.0 / 4) * (2 * matriz[i - 1][j] + 2 * matriz[i][j + 1] - 4 * matriz[i][j]) * w
+            n = matriz[i][j] + (1.0 / 4) * (2 * matriz[i - 1][j] + 2 * matriz[i][j + 1] - 4 * matriz[i][j]
+                                            - (h ** 2) * ro) * w
+
         elif i == height and j == width:  # esquina inferior derecha
-            n = matriz[i][j] + (1.0 / 4) * (2 * matriz[i - 1][j] + 2 * matriz[i][j - 1] - 4 * matriz[i][j]) * w
+            n = matriz[i][j] + (1.0 / 4) * (2 * matriz[i - 1][j] + 2 * matriz[i][j - 1] - 4 * matriz[i][j]
+                                            - (h ** 2) * ro) * w
         # Bordes
         else:
-            n = matriz[i][j]  # Como son dirichlet no se hace nada
-    e = abs(matriz[i, j] - n)
+            return matriz[i][j], e  # Como son dirichlet no se hace nada
 
-    if np.isnan(e) or e == 0.0:
-        e = 1
-    return (n, e)
-
-
-def rho(x, y, b=False):
-    if b:
-        den = math.sqrt((x ** 2) + (y ** 2) + 120)
-        return 1 / den
+    if n == matriz[i][j]:
+        return matriz[i][j], e
     else:
-        return 0
+        e = abs(matriz[i][j] - n)
+    return n, e
 
 
-def plot(matrix):
-    fig = plt.figure()
-    cax = plt.imshow(matrix, origin="lower")
-    cb = fig.colorbar(cax)
-
-    plt.show()
+def rho(x, y):
+    return 1 / (x**2 + y**2 + 120)**0.5
 
 
 def f(p1, p2, x):
@@ -310,13 +350,24 @@ def get_w(m, n):
 
 
 def main():
-    t = Tarea(dh=40)
+    t = Tarea(dh=10)
     t.set_geo()
+    t.cb(0)
+    t.iterate(b=True)
+    t.plot_temp()
+    print(np.shape(t.get_temp_matrix()))
     t.cb(8)
-    m = t.get_temp_matrix()
-    t.iterate(b=False)
-    print m.shape
-    plot(m)
+    t.iterate(b=True)
+    t.plot_temp()
+    t.cb(12)
+    t.iterate(b=True)
+    t.plot_temp()
+    t.cb(16)
+    t.iterate(b=True)
+    t.plot_temp()
+    t.cb(20)
+    t.iterate(b=True)
+    t.plot_temp()
 
 
 if __name__ == '__main__':
